4833ee006a355196492bfcb1763276fffa26c979
klacointe <kevinlacointe@gmail.com>
2014-09-26 15:10:12 +0200

    [VCR] Record new episodes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ RSpec.configure do |config|
 
   VCR.configure do |c|
     c.cassette_library_dir = 'spec/fixtures/vcr'
+    c.default_cassette_options = { record: :new_episodes }
     c.hook_into :webmock
     c.allow_http_connections_when_no_cassette = true
     c.configure_rspec_metadata!
