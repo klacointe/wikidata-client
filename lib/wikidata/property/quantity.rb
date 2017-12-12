2bc841834a6d8fa3a8b5177cbe95bf2a225332e5
KimBue <kimbuergl@gmail.com>
2017-12-12 18:04:31 +0100

    changed Quantity from string to float

--- lib/wikidata/property/quantity.rb
+++ lib/wikidata/property/quantity.rb
@@ -2,13 +2,13 @@ module Wikidata
   module Property
     class Quantity < Wikidata::Property::Base
       def amount
-        value.amount
+        value.amount.to_f
       end
       def upperBound
-        value.upperBound
+        value.upperBound.to_f
       end
       def lowerBound
-        value.lowerBound
+        value.lowerBound.to_f
       end
       def unit
         value.unit
